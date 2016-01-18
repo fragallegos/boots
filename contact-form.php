c3da0c4f9e24b06091339af3ad21db17d50af4f8
Andres Gallegos <fragallegos@me.com>
2016-01-18 13:24:37 -0500

    testy

---
+++ contact-form.php
@@ -0,0 +1,48 @@
+<?php
+
+//Add your information here
+$recipient = "your_mail@domain.com";
+$successPage = "index.html";
+
+//Don't edit anything below this line
+
+
+//import form information
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$name=stripslashes($name);
+$email=stripslashes($email);
+$subject=stripslashes($subject);
+$message=stripslashes($message);
+$message= "Name: $name, \n\n Message: $message";
+
+/*
+Simple form validation
+check to see if an email and message were entered
+*/
+
+//if no message entered and no email entered print an error
+if (empty($message) && empty($email)){
+print "No email address and no message was entered. <br>Please include an email and a message";
+}
+//if no message entered send print an error
+elseif (empty($message)){
+print "No message was entered.<br>Please include a message.<br>";
+}
+//if no email entered send print an error
+elseif (empty($email)){
+print "No email address was entered.<br>Please include your email. <br>";
+}
+//if the form has both an email and a message
+else {
+
+//mail the form contents
+mail( "$recipient", "$subject", "$message", "From: $email" );
+header("Location: $successPage");
+}
+
+?>
